349c9fd0469f18a4d95532b8fed00c764c96e576
Abdul Rehman <iamabyes10@gmail.com>
2025-10-04 00:41:16 +0500

    hehe

--- src/api/routes/group.routes.ts
+++ src/api/routes/group.routes.ts
@@ -5,10 +5,13 @@ const groupRouter = Router();
 
 groupRouter.post('/', groupController.createGroup);
 groupRouter.get('/', groupController.getAllGroups);
+//added
+groupRouter.get('/user/:userId', groupController.getGroupsByUser);
+//
 groupRouter.get('/:id', groupController.getGroupById);
 groupRouter.put('/:id', groupController.updateGroup);
 groupRouter.delete('/:id', groupController.deleteGroup);
 //added
-groupRouter.get("/groups/user/:userId", groupController.getGroupsByUser);
+// groupRouter.get("/groups/user/:userId", groupController.getGroupsByUser);
 
 export default groupRouter; 
